f73999189d49955010430233317411d01657f828
James Hansen <jameszenartist@gmail.com>
2024-01-20 03:08:26 -0800

    update origins w/ favicon.ico path

--- server/config/origins.js
+++ server/config/origins.js
@@ -1,6 +1,7 @@
 const Origins = [
   "https://fanciful-pothos-1e84ca.netlify.app",
   "https://good-gray-pike-kit.cyclic.app",
+  "https://good-gray-pike-kit.cyclic.app/favicon.ico",
 ];
 
 module.exports = Origins;
