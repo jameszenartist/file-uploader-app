4cc8e2632278b060d4c549355b159e2527779347
James Hansen <jameszenartist@gmail.com>
2024-01-20 01:19:46 -0800

    update origins

--- server/config/origins.js
+++ server/config/origins.js
@@ -1,7 +1,6 @@
 const Origins = [
   "https://fanciful-pothos-1e84ca.netlify.app",
-  "http://good-gray-pike-kit.cyclic.app/",
-  "https://good-gray-pike-kit.cyclic.app/",
+  "https://good-gray-pike-kit.cyclic.app",
 ];
 
 module.exports = Origins;
