e2f1ce50b6c4b3d26b7aaa55f8d76635726ce2b6
James Hansen <jameszenartist@gmail.com>
2024-07-06 05:06:57 -0700

    temp update cronjob middleware

--- server/middleware/limitCron.js
+++ server/middleware/limitCron.js
@@ -1,5 +1,15 @@
 const limitCron = (req, res, next) => {
-  if (req.headers["user-agent"] === "Cyclic-Cron") {
+  // if (req.headers["user-agent"] === "Cyclic-Cron") {
+  //   next();
+  // } else {
+  //   return res.status(403).json({ error: `Forbidden cron job request` });
+  // }
+
+  if (req.headers["user-agent"]) {
+    console.log(
+      "cronjob middleware, user-agent is: ",
+      req.headers["user-agent"]
+    );
     next();
   } else {
     return res.status(403).json({ error: `Forbidden cron job request` });
